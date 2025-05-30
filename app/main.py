80b9430bd1e44a6569d8aa5eabcf33067c954142
Inês <inessmatos@ua.pt>
2025-05-30 15:58:22 +0100

    main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
-
-def sum(a, b):
-    return a + b
+from data_storage import create_db, insert_log
+from aggregator_core import calculate_metrics
+from datetime import datetime
 
 def main():
-    print("Hello, Aggregator!")
-    
+    create_db()  
+
+    # Simulated input data
+    dummy_data = {
+        "station_id": "WS_1",
+        "timestamp": datetime.now().isoformat(),
+        "assembly_time": 45.2,
+        "defect_count": 1,
+        "defect_type": "misalignment",
+        "success": False
+    }
+
+    insert_log(dummy_data)
+
+    # Show metrics
+    metrics = calculate_metrics()
+    print("Aggregated metrics:", metrics)
+
 if __name__ == "__main__":
     main()
+
